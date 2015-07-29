c3c41e0d2400fbb4d5255f91308b5c1b68b7b6c0
Brent Pedersen (brentp) <bpederse@gmail.com>
2015-07-29 16:23:19 -0600

    order stuff

--- effect.py
+++ effect.py
@@ -6,33 +6,39 @@ class Effect(object):
 
     def __init__(self, effect_dict):
         # or maybe arg should be a dict for Effect()
-        raise NotImplemented
+        raise NotImplementedError
 
     def __le__(self, other):
         if self.pseudogene and not other.pseudogene:
-            return other.severity
+            return True
+        elif other.pseudogene and not self.pseudogene:
+            return False
+        if self.coding and not other.coding:
+            return False
+        elif other.coding and not self.coding:
+            return True
         return self.severity <= other.severity
 
     def __eq__(self, other):
-        raise NotImplemented
+        raise NotImplementedError
 
     def __str__(self):
-        raise NotImplemented
+        raise NotImplementedError
 
     def __repr__(self):
-        raise NotImplemented
+        raise NotImplementedError
 
     @property
     def gene(self):
-        raise NotImplemented
+        raise NotImplementedError
 
     @property
     def transcript(self):
-        raise NotImplemented
+        raise NotImplementedError
 
     @property
     def exon(self):
-        raise NotImplemented
+        raise NotImplementedError
 
     @property
     def coding(self):
@@ -44,11 +50,11 @@ class Effect(object):
 
     @property
     def aa_change(self):
-        raise NotImplemented
+        raise NotImplementedError
 
     @property
     def codon_change(self):
-        raise NotImplemented
+        raise NotImplementedError
 
     @property
     def severity(self):
@@ -57,15 +63,15 @@ class Effect(object):
 
     @property
     def consequence(self):
-        raise NotImplemented
+        raise NotImplementedError
 
     @property
     def biotype(self):
-        raise NotImplemented
+        raise NotImplementedError
 
     @property
     def is_pseudogene(self): #bool
-        raise NotImplemented
+        raise NotImplementedError
 
 class SnpEff(Effect):
 
@@ -87,11 +93,28 @@ class SnpEff(Effect):
 
     @property
     def consequence(self):
+        if '&' in self.effects['Annotation']:
+            return self.effects['Annotation'].split('&')
         return self.effects['Annotation']
 
+    @property
+    def alt(self):
+        return self.effects['Allele']
+
+    @property
+    def is_pseudogene(self):
+        return 'pseudogene' in self.effects['Transcript_BioType']
+
+    @property
+    def coding(self):
+        return self.effects['Transcript_BioType'] == 'protein_coding'
 
 if __name__ == "__main__":
 
     s = SnpEff("A|stop_gained|HIGH|C1orf170|ENSG00000187642|transcript|ENST00000433179|protein_coding|3/5|c.262C>T|p.Arg88*|262/3064|262/2091|88/696||")
     print s.effects
-    print s.gene, s.transcript, s.consequence
+    print s.gene, s.transcript, s.consequence, s.is_pseudogene
+    s = SnpEff("G|splice_donor_variant&intron_variant|HIGH|WASH7P|ENSG00000227232|transcript|ENST00000423562|unprocessed_pseudogene|6/9|n.822+2T>C||||||")
+    print s.is_pseudogene
+    s = SnpEff("G|missense_variant|MODERATE|OR4F5|ENSG00000186092|transcript|ENST00000335137|protein_coding|1/1|c.338T>G|p.Phe113Cys|338/918|338/918|113/305||")
+    print s.coding, s.consequence
